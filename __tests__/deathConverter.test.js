33be31ebcc1180a8488e421846c537f0243adf60
dcouch440 <dcouch440@gmail.com>
2021-02-05 15:48:04 -0800

    fix bracket spacing line 4

--- __tests__/deathConverter.test.js
+++ __tests__/deathConverter.test.js
@@ -1,7 +1,7 @@
 import {deathConverter} from '../src/js/utils.js';
 
 describe('deathConverter', () => {
-  test("It should return the years the person has to live and convert it to the solar age.", () =>{
+  test("It should return the years the person has to live and convert it to the solar age.", () => {
     expect(deathConverter(18, 1, 80)).toEqual('You have 62 years to live.');
   });
   test("It should return an alternate sentence if the person is living past their expected life span", () => {
