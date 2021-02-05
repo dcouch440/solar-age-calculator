36048a0f52cb9586e4b2116eb3d82b9726c0aaab
dcouch440 <dcouch440@gmail.com>
2021-02-05 15:42:04 -0800

    TEST => .deathConverter() => seperate test PASS

--- __tests__/deathConverter.test.js
+++ __tests__/deathConverter.test.js
@@ -1,8 +1,10 @@
-import {deathConverter} from '../src/js/utils.js'
+import {deathConverter} from '../src/js/utils.js';
 
 describe('deathConverter', () => {
-  test("It should take three arguments and find if its value is negative or positive. It should return two different values depending on such. In addition to that it will return the value it used to calculate that.", () =>{
+  test("It should return the years the person has to live and convert it to the solar age.", () =>{
     expect(deathConverter(18, 1, 80)).toEqual('You have 62 years to live.');
+  });
+  test("It should return an alternate sentence if the person is living past their expected life span", () => {
     expect(deathConverter(1, 0.24, 0)).toEqual('You lived 4 years past expected!');
   });
 });
