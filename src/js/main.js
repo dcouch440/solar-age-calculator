9d4957e36d5d71e4dae4d4ed24c4dc09b951f6be
dcouch440 <dcouch440@gmail.com>
2021-02-05 11:59:35 -0800

    add inner html for data from class

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,27 @@
 import '../css/styles.css';
 import {SolarAge} from './SolarAge.js';
 
-// const eventLogger = (event, age) => {
-
-//   const currentEvent = 
-//   {
-//     Earth: () =>  age.getEarthAge(),
-//     Mercury: () => age.getMercuryAge(),
-//     Venus: () => age.getVenusAge(),
-//     Mars: () => age.getMarsAge(),
-//     Jupiter: () => age.getJupiterAge(),
-//     Default: () => null
-//   };
-//   return (currentEvent['cats']() || currentEvent['Default']());
-// };
-
-SolarAge;
+const valueLogger = (value, solar) => {
+  const currentValue = 
+  {
+    Earth: () =>  solar.getEarthAge(),
+    Mercury: () => solar.getMercuryAge(),
+    Venus: () => solar.getVenusAge(),
+    Mars: () => solar.getMarsAge(),
+    Jupiter: () => solar.getJupiterAge(),
+    Default: () => null
+  };
+  return (currentValue[value]() || currentValue['Default']());
+};
 document.addEventListener("DOMContentLoaded", () => {
-  const dropDownValue = document.getElementById('options').value;
-
+  const output = document.getElementById('output');
+  
+  document.getElementById('submit').addEventListener('click', () => 
+  {
+    const dropDownValue = document.getElementById('options').value;
+    const age = document.getElementById('age').value;
+    const death = document.getElementById('death').value;
+    const solarAge = new SolarAge(age, death);
+    output.innerHTML = valueLogger(dropDownValue, solarAge);
+  });
 });
