0880103d585ebdf344cdf570147c3dbc955ab26d
dcouch440 <dcouch440@gmail.com>
2021-02-05 11:47:22 -0800

    add event logger for dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 import '../css/styles.css';
+import {SolarAge} from './SolarAge.js';
 
+// const eventLogger = (event, age) => {
+
+//   const currentEvent = 
+//   {
+//     Earth: () =>  age.getEarthAge(),
+//     Mercury: () => age.getMercuryAge(),
+//     Venus: () => age.getVenusAge(),
+//     Mars: () => age.getMarsAge(),
+//     Jupiter: () => age.getJupiterAge(),
+//     Default: () => null
+//   };
+//   return (currentEvent['cats']() || currentEvent['Default']());
+// };
+
+SolarAge;
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const dropDownValue = document.getElementById('options').value;
+
 });
