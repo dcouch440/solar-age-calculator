dc9d5f64862eeb21089c54a1bc309704ce3507e1
dcouch440 <dcouch440@gmail.com>
2021-02-05 14:29:46 -0800

    TEST => ageConverter() => RED

---
+++ __tests__/ageConverter.test.js
@@ -0,0 +1,8 @@
+import {ageConverter} from '../src/js/utils.js';
+
+describe('ageConverter', () => {
+  test("It will divide a given value by a given value. In this case, a float which is being used as a rate", () => {
+    expect(ageConverter(18, 80)).toEqual(75);
+  });
+});
+
