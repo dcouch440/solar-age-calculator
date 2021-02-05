7d71c5dd725f1fdb559c574bcd6c5dd5a20505f1
dcouch440 <dcouch440@gmail.com>
2021-02-05 15:13:56 -0800

    TEST => deathConverter => GREEN  PASS

--- __tests__/deathConverter.test.js
+++ __tests__/deathConverter.test.js
@@ -2,8 +2,7 @@ import {deathConverter} from '../src/js/utils.js'
 
 describe('deathConverter', () => {
   test("It should take three arguments and find if its value is negative or positive. It should return two different values depending on such. In addition to that it will return the value it used to calculate that.", () =>{
-    expect(deathConverter(18, 0.24, 80)).toEqual('You have 62 years to live.');
+    expect(deathConverter(18, 1, 80)).toEqual('You have 62 years to live.');
     expect(deathConverter(1, 0.24, 0)).toEqual('You lived 4 years past expected!');
   });
-  
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,2 +1,10 @@
 export const ageConverter = (age, rate) => Math.floor(age / rate);
-export const deathConverter = () => 'test';
+
+export const deathConverter = (age, rate, death) => {
+  const total = Math.floor(death / rate) - Math.floor(age / rate);
+  if (total > 0) {
+    return `You have ${total} years to live.`;
+  } else {
+    return `You lived ${total * -1} years past expected!`;
+  }
+};
