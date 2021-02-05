b6dc090f888ce9be3badc443cc6903f73f4df3b0
dcouch440 <dcouch440@gmail.com>
2021-02-05 09:25:28 -0800

    SolarAge => name change => PASS ALL

--- __tests__/SolarAge.test.js
+++ __tests__/SolarAge.test.js
@@ -1,17 +1,17 @@
 import {SolarAge} from '../src/js/SolarAge.js'
 
 describe("SolarAge", () => {
-  let age;
+  let solarAge;
   beforeEach(() => {
-    age = new SolarAge(18);
+    solarAge = new SolarAge(18);
   });
   test("It will take an age (integer) and store its value in an object", () => {
-    expect(age.years).toEqual(18);
+    expect(solarAge.years).toEqual(18);
   });
   test("It will return the age in Earth earth years", () => {
-    expect(age.getEarthAge()).toEqual(18);
+    expect(solarAge.getEarthAge()).toEqual(18);
   });
   test("It will return the age in Mercury years", () => {
-    expect(age.getMercuryAge()).toEqual(75);
+    expect(solarAge.getMercuryAge()).toEqual(75);
   });
 });
