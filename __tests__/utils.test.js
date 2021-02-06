eef26f5973651e7d4fce98489a276d80c193fc02
dcouch440 <dcouch440@gmail.com>
2021-02-06 14:21:18 -0800

    RE-TEST => deathConverter() => new file PASS

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -8,11 +8,10 @@ describe('ageConverter', () => {
 
 describe('deathConverter', () => {
   test("It should return the years the person has to live and convert it to the solar age.", () => {
-    expect(deathConverter(18, 1, 80)).toEqual(null);
-    // 'You have 62 years to live.' 
+    expect(deathConverter(18, 1, 80)).toEqual('You have 62 years to live.' );
+
   });
   test("It should return an alternate sentence if the person is living past their expected life span", () => {
-    expect(deathConverter(1, 0.24, 0)).toEqual(null);
-    // 'You lived 4 years past expected!'
+    expect(deathConverter(1, 0.24, 0)).toEqual('You lived 4 years past expected!');
   });
 });
