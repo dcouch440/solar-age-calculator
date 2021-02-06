fae5d2ac9472cb11a4505c283f90617b3bc6da77
dcouch440 <dcouch440@gmail.com>
2021-02-06 14:18:24 -0800

    RE-TEST => ageConverter() => new file PASS

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,8 +2,7 @@ import {ageConverter, deathConverter} from '../src/js/utils.js';
 
 describe('ageConverter', () => {
   test("It will divide a given value by a given value. In this case, a float which is being used as a rate", () => {
-    expect(ageConverter(18, 0.24)).toEqual(null);
-    // 75
+    expect(ageConverter(18, 0.24)).toEqual(75);
   });
 });
 
