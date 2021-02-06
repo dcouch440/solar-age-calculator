3bc843a58f136a284814b2a155c2f498818bbe40
dcouch440 <dcouch440@gmail.com>
2021-02-06 14:16:24 -0800

    TEST => agecoverter() => RED

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,15 +2,18 @@ import {ageConverter, deathConverter} from '../src/js/utils.js';
 
 describe('ageConverter', () => {
   test("It will divide a given value by a given value. In this case, a float which is being used as a rate", () => {
-    expect(ageConverter(18, 0.24)).toEqual(75);
+    expect(ageConverter(18, 0.24)).toEqual(null);
+    // 75
   });
 });
 
 describe('deathConverter', () => {
   test("It should return the years the person has to live and convert it to the solar age.", () => {
-    expect(deathConverter(18, 1, 80)).toEqual('You have 62 years to live.');
+    expect(deathConverter(18, 1, 80)).toEqual(null);
+    // 'You have 62 years to live.'
   });
   test("It should return an alternate sentence if the person is living past their expected life span", () => {
-    expect(deathConverter(1, 0.24, 0)).toEqual('You lived 4 years past expected!');
+    expect(deathConverter(1, 0.24, 0)).toEqual(null);
+    // 'You lived 4 years past expected!'
   });
 });
